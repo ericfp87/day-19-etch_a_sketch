04e9ca35ad6f2e866f11a925335541e63ba5329f
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-03-01 09:59:13 -0300

    first

--- main.py
+++ main.py
@@ -2,25 +2,32 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
-
+tim.speed("fastest")
 
 def forwarding():
     tim.forward(10)
 
 def lefting():
-    tim.left(1)
+    tim.left(10)
 
 def righting():
-    tim.right(1)
+    tim.right(10)
 
 def backing():
     tim.back(10)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 
 screen.onkeypress(forwarding, "w")
 screen.onkeypress(lefting, "a")
 screen.onkeypress(righting, "d")
 screen.onkeypress(backing, "s")
+screen.onkey(clear, "c")
 screen.listen()
 
 screen.exitonclick()
