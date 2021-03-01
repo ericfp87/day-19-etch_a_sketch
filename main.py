685c94526480ccdae2e0f87cf0f2fc73fda495cc
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-03-01 09:58:55 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def forwarding():
+    tim.forward(10)
+
+def lefting():
+    tim.left(1)
+
+def righting():
+    tim.right(1)
+
+def backing():
+    tim.back(10)
+
+
+screen.onkeypress(forwarding, "w")
+screen.onkeypress(lefting, "a")
+screen.onkeypress(righting, "d")
+screen.onkeypress(backing, "s")
+screen.listen()
+
+screen.exitonclick()
+
